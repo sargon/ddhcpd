1e9736c5902617c83fbe83b156e7a757b1a251cb
Benny Baumann <BenBE@geshi.org>
2017-01-09 23:15:14 +0100

    fix: Comment on the magic numbers used

--- ddhcp.c
+++ ddhcp.c
@@ -248,7 +248,10 @@ void del_fd(int efd, int fd, uint32_t events) {
 }
 
 uint32_t get_loop_timeout(ddhcp_config* config) {
-  return floor(config->tentative_timeout / 2 * 1000);
+  //Multiply by 500 to convert the timeout value given in seconds
+  //into milliseconds AND dividing the value by two at the same time.
+  //The integer overflow occuring for timeouts greater than 99.4 days is ignored here.
+  return floor(config->tentative_timeout * 500);
 }
 
 typedef void (*sighandler_t)(int);
