4a4625e537e6ccdc80f30c5cc93d0bdc7c2bb08a
Benny Baumann <BenBE@geshi.org>
2017-01-19 23:16:56 +0100

    fix: Avoid stack buffer overflow when struct in_addr is larger than addr

--- tools.c
+++ tools.c
@@ -3,10 +3,10 @@
 #include <string.h>
 
 void addr_add(struct in_addr* subnet, struct in_addr* result, int add) {
-  uint32_t addr;
+  struct in_addr addr;
   memcpy(&addr, subnet, sizeof(struct in_addr));
-  addr = ntohl(addr);
-  addr += add;
-  addr = htonl(addr);
+  addr.s_addr = ntohl(addr.s_addr);
+  addr.s_addr += add;
+  addr.s_addr = htonl(addr.s_addr);
   memcpy(result, &addr, sizeof(struct in_addr));
 }
