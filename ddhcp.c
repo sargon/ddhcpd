6356dcf562dfbfd8313e931d4400f8300fa90d95
Benny Baumann <BenBE@geshi.org>
2017-01-23 22:40:05 +0100

    add: Explicit length checks forcontrol frames received

--- ddhcp.c
+++ ddhcp.c
@@ -300,11 +300,21 @@ int handle_command(int socket, uint8_t* buffer, int msglen, ddhcp_block* blocks,
   // TODO Avoid magic numbers for commands
   switch (buffer[0]) {
   case 1:
+    if (msglen != 1) {
+      DEBUG("handle_command(...) -> message length mismatch\n");
+      return -2;
+    }
+
     DEBUG("handle_command(...) -> show block status\n");
     block_show_status(socket, blocks, config);
     return 0;
 
   case 2:
+    if (msglen != 1) {
+      DEBUG("handle_command(...) -> message length mismatch\n");
+      return -2;
+    }
+
     DEBUG("handle_command(...) -> show dhcp options\n");
     dhcp_options_show(socket, &config->options);
     return 0;
@@ -312,20 +322,26 @@ int handle_command(int socket, uint8_t* buffer, int msglen, ddhcp_block* blocks,
   case 3:
     DEBUG("handle_command(...) -> set dhcp option\n");
 
-    if (msglen > 3) {
-      dhcp_option* option = (dhcp_option*) calloc(sizeof(dhcp_option), 1);
-      option->code = buffer[1];
-      option->len = buffer[2];
-      printf("%i:%i\n", buffer[1], buffer[2]);
-      option->payload = (uint8_t*)  calloc(sizeof(uint8_t), option->len);
-      memcpy(option->payload, buffer + 3, option->len);
-
-      set_option_in_store(&config->options, option);
-      return 0;
-    } else {
+    if (msglen < 3) {
       DEBUG("handle_command(...) -> message not long enought\n");
       return -2;
     }
+
+    if(buffer[2] > msglen - 3) {
+      DEBUG("handle_command(...) -> message not long enought\n");
+      return -2;
+    }
+
+    dhcp_option* option = (dhcp_option*) calloc(sizeof(dhcp_option), 1);
+    option->code = buffer[1];
+    option->len = buffer[2];
+    printf("%i:%i\n", buffer[1], buffer[2]);
+    option->payload = (uint8_t*) calloc(sizeof(uint8_t), option->len);
+
+    memcpy(option->payload, buffer + 3, option->len);
+
+    set_option_in_store(&config->options, option);
+    return 0;
 
   default:
     WARNING("handle_command(...) -> unknown command\n");
