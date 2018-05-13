9fd32f274489fe6d3dd789e1afd5aa8c59f83a7f
Daniel Ehlers <danielehlers@mindeye.net>
2018-05-13 20:15:19 +0200

    Fixup debug message

--- hook.c
+++ hook.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 
 void hook(uint8_t type, struct in_addr* address, uint8_t* chaddr, ddhcp_config* config) {
-  DEBUG("hook(%i,%s,%s,config)\n", inet_ntoa(*address), hwaddr2c(chaddr), 1);
+  DEBUG("hook(%i,%s,%s,config)\n",type, inet_ntoa(*address), hwaddr2c(chaddr));
 
   if (config->hook_command) {
     DEBUG("hook( ... ): No hook command set");
